6a491783cda4849c3cb25e7db9d1874b6b6d5971
Susanna Martinelli <susanna.martinelli@gmail.com>
2019-01-13 15:42:04 +0100

    testing with docker

--- httptest/httptest.go
+++ httptest/httptest.go
@@ -10,16 +10,13 @@ import (
 
 func TestHealthCheckHandler(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/health-check", nil)
-
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(HealthCheckHandler)
 	handler.ServeHTTP(rr, req)
-
 	// Check the status code is what we expect.
 	if rr.Code != http.StatusOK {
 		t.Errorf("wrong status code: got %v want %v", status, http.StatusOK)
 	}
-
 	// Check the response body is what we expect.
 	if rr.Body.String() != `{"alive": true}` {
 		t.Errorf("Unexpected body: got %v want %v", rr.Body.String(), expected)
