09fd6e82c2257b522bdfae39f062b245e60c284b
Susanna Martinelli <susanna.martinelli@gmail.com>
2019-01-19 15:58:54 +0100

    fixing networking slides

--- httptest/httptest.go
+++ httptest/httptest.go
@@ -1,23 +1,27 @@
 package httptest
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 // START OMIT
+//we want to test
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `{"alive": true}`)
+}
 
+//our test
 func TestHealthCheckHandler(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/health-check", nil)
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(HealthCheckHandler)
 	handler.ServeHTTP(rr, req)
-	// Check the status code is what we expect.
-	if rr.Code != http.StatusOK {
-		t.Errorf("wrong status code: got %v want %v", status, http.StatusOK)
-	}
-	// Check the response body is what we expect.
+
 	if rr.Body.String() != `{"alive": true}` {
 		t.Errorf("Unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
